e2f578492e62741ba713bf0e5865de09622b3ced
GuilhermeJaime <guilhermejaime16@gmail.com>
2025-08-10 07:49:57 +0100

    novo exercício

---
+++ aula14ex/scripts.js
@@ -0,0 +1,39 @@
+function contar() {
+  var ini = document.getElementById("txti")
+  var fim = document.getElementById("txtf")
+  var passo = document.getElementById("txtp")
+  var res = document.getElementById("res")
+
+  if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    window.alert('Preencha todos os campos!')
+  }
+  else {
+    res.innerHTML = 'Contando: <br>'
+    var i = Number(ini.value)
+    var f = Number(fim.value)
+    var p = Number(passo.value)
+    if (p <= 0) {
+      window.alert('Passo inválido! Considerando PASSO 1')
+      p = 1
+    }
+
+    if (p > 0) {
+      if (i < f) {
+        // Contagem crescente
+        for (let c = i; c <= f; c += p) {
+          res.innerHTML += `${c} \u{1F449} `
+        }
+      } else {
+        // Contagem decrescente
+        for (let c = i; c >= f; c -= p) {
+          res.innerHTML += `${c} \u{1F449} `
+        }
+        res.innerHTML += `\u{1F3C1}` // Finish flag emoji
+
+      }
+    }
+    res.innerHTML += `\u{1F3C1}` // Emoji de bandeira de chegada
+
+  }
+
+}
